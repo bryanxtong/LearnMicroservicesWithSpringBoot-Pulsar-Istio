a8d607b2f3abf5005471eeda27b555eb493c2b0e
bryanxtong <bryanxtong@gmail.com>
2026-02-02 18:26:35 +0800

    update to spring boot 4.0.2 and spring cloud 2025.1.0

--- multiplication/src/main/java/microservices/book/multiplication/MultiplicationApplication.java
+++ multiplication/src/main/java/microservices/book/multiplication/MultiplicationApplication.java
@@ -1,7 +1,5 @@
 package microservices.book.multiplication;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
